ce920c219dce3c1c639b2732a874cdc63ff1a708
20211tiimi0303 <cesbarbosa2005@gmail.com>
2023-06-02 08:37:51 -0300

    tela de login iniciada

---
+++ app/src/main/java/carlos/dara/kaua/raynan/reciclamais/activity/LoginActivity.java
@@ -0,0 +1,18 @@
+package carlos.dara.kaua.raynan.reciclamais.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import carlos.dara.kaua.raynan.reciclamais.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
