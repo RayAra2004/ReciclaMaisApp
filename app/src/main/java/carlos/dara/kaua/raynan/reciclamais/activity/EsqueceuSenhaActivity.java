6d6cd166d69d7e2232e214dc26e317af6cd2c13f
RayAra2004 <raynan.araujo@gmail.com>
2023-06-16 19:20:01 -0300

    Criação arquivos: esqueceuSenha, seusMateriais,

---
+++ app/src/main/java/carlos/dara/kaua/raynan/reciclamais/activity/EsqueceuSenhaActivity.java
@@ -0,0 +1,16 @@
+package carlos.dara.kaua.raynan.reciclamais.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import carlos.dara.kaua.raynan.reciclamais.R;
+
+public class EsqueceuSenhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_esqueceu_senha);
+    }
+}
