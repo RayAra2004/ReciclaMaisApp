f19f21f267aaeab9e307ff46dc3e38ba23e31d03
20211TIIMI0133 <raynan.araujo@gmail.com>
2023-06-16 08:52:43 -0300

    ToolBar

--- app/src/main/java/carlos/dara/kaua/raynan/reciclamais/activity/AjudaActivity.java
+++ app/src/main/java/carlos/dara/kaua/raynan/reciclamais/activity/AjudaActivity.java
@@ -1,6 +1,8 @@
 package carlos.dara.kaua.raynan.reciclamais.activity;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -11,6 +13,12 @@ public class AjudaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R   .layout.activity_ajuda);
+        setContentView(R.layout.activity_ajuda);
+
+        Toolbar toolbar = findViewById(R.id.tbAjuda);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
