bc16432baaa105b204276d7fd7ec1d864e8ad5da
kaua <kaua271203@gmail.com>
2023-07-07 06:34:52 -0300

    tirando arquivos errados

--- app/src/main/java/carlos/dara/kaua/raynan/reciclamais/activity/MainActivity.java
+++ app/src/main/java/carlos/dara/kaua/raynan/reciclamais/activity/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item){
         switch (item.getItemId()){
-            case R.id.logo:
+            case R.drawable.reciclar:
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
