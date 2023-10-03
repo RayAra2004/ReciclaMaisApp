5d2c7b3de9a67ac6234ca4738725ac2e533d0b87
RayAra2004 <raynan.araujo@gmail.com>
2023-10-03 09:05:04 -0300

    Trocando activity pra fragment e criando entidades

---
+++ app/src/main/java/carlos/dara/kaua/raynan/reciclamais/entities/Endereco.java
@@ -0,0 +1,27 @@
+package carlos.dara.kaua.raynan.reciclamais.entities;
+
+import java.math.BigInteger;
+
+public class Endereco {
+    BigInteger cep;
+    String tp_logradouro;
+    String logradouro;
+    int numero;
+    String estado;
+    String cidade;
+    String bairro;
+
+    Double lat, lon;
+
+    public Endereco(BigInteger cep, String tp_logradouro, String logradouro, int numero, String estado, String cidade, String bairro, Double lat, Double lon) {
+        this.cep = cep;
+        this.tp_logradouro = tp_logradouro;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.lat = lat;
+        this.lon = lon;
+    }
+}
---
+++ app/src/main/java/carlos/dara/kaua/raynan/reciclamais/entities/PontoColeta.java
@@ -0,0 +1,28 @@
+package carlos.dara.kaua.raynan.reciclamais.entities;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class PontoColeta {
+    String nome;
+    int imagem;
+
+    Double nota;
+    Double lat, lon;
+
+    BigInteger telefone;
+    ArrayList<String> materiasReciclados;
+
+    Endereco endereco;
+
+    public PontoColeta(String nome, int imagem, Double nota, Double lat, Double lon, BigInteger telefone, ArrayList<String> materiasReciclados, Endereco endereco) {
+        this.nome = nome;
+        this.imagem = imagem;
+        this.nota = nota;
+        this.lat = lat;
+        this.lon = lon;
+        this.telefone = telefone;
+        this.materiasReciclados = materiasReciclados;
+        this.endereco = endereco;
+    }
+}
