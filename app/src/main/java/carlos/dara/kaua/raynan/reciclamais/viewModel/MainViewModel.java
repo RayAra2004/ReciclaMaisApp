3575fc08a3ba403bedb03aa967fd3d2be9e0bf83
RayAra2004 <raynan.araujo@gmail.com>
2023-11-28 00:13:53 -0300

    adicionar ponto início

--- app/src/main/java/carlos/dara/kaua/raynan/reciclamais/viewModel/MainViewModel.java
+++ app/src/main/java/carlos/dara/kaua/raynan/reciclamais/viewModel/MainViewModel.java
@@ -1,5 +1,9 @@
 package carlos.dara.kaua.raynan.reciclamais.viewModel;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
@@ -7,9 +11,13 @@ import java.util.ArrayList;
 import carlos.dara.kaua.raynan.reciclamais.R;
 import carlos.dara.kaua.raynan.reciclamais.entities.PontoColeta;
 
-public class MainViewModel extends ViewModel {
+public class MainViewModel extends AndroidViewModel {
     int navigationOpSelected = R.id.homeOP;
-    ArrayList<PontoColeta> pontos = new ArrayList<>();
+    String currentPhotoPath = "";
+
+    public MainViewModel(@NonNull Application application){ super(application);}
+
+    /*ArrayList<PontoColeta> pontos = new ArrayList<>();
     PontoColeta pontoColeta;
     public ArrayList<PontoColeta> getPontos() {
         //PontoColeta p1 = new PontoColeta();
@@ -19,8 +27,18 @@ public class MainViewModel extends ViewModel {
 
     public void setPontos(ArrayList<PontoColeta> pontos) {
         this.pontos = pontos;
+    }*/
+
+    /* ADICIONAR PONTO VIEW MODEL*/
+    public String getCurrentPhotoPath() {
+        return currentPhotoPath;
     }
 
+    public void setCurrentPhotoPath(String currentPhotoPath) {
+        this.currentPhotoPath = currentPhotoPath;
+    }
+
+    /* NAVEGAÇÃO ENTRE TELAS */
     public int getNavigationOpSelected(){
         return navigationOpSelected;
     }
