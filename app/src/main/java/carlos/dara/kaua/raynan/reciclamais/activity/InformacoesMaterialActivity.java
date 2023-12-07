e1001c5e64b021ab5de246e28610f59fdad72c97
CarlosBarbosa2005 <cesbarbosa2005@gmail.com>
2023-12-07 20:22:48 -0300

    Informações como reciclar

--- app/src/main/java/carlos/dara/kaua/raynan/reciclamais/activity/InformacoesMaterialActivity.java
+++ app/src/main/java/carlos/dara/kaua/raynan/reciclamais/activity/InformacoesMaterialActivity.java
@@ -1,9 +1,8 @@
 package carlos.dara.kaua.raynan.reciclamais.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.widget.ImageView;
 import carlos.dara.kaua.raynan.reciclamais.R;
 
 public class InformacoesMaterialActivity extends AppCompatActivity {
@@ -12,5 +11,43 @@ public class InformacoesMaterialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacoes_material);
+
+        ImageView imvBannerMaterial = findViewById(R.id.imv_banner_materiais);
+
+        // Obtém o banner da Intent
+        String banner = getIntent().getStringExtra("banner");
+
+        // Define a imagem com base no banner
+        if (banner != null) {
+            switch (banner) {
+                case "BannerVidro":
+                    imvBannerMaterial.setImageResource(R.drawable.banner_vidro);
+                    break;
+                case "BannerPlastico":
+                    imvBannerMaterial.setImageResource(R.drawable.banner_plastico);
+                    break;
+                case "BannerMadeira":
+                    imvBannerMaterial.setImageResource(R.drawable.banner_madeira);
+                    break;
+                case "BannerEletronico":
+                    imvBannerMaterial.setImageResource(R.drawable.banner_eletronicos);
+                    break;
+                case "BannerMetal":
+                    imvBannerMaterial.setImageResource(R.drawable.banner_metal);
+                    break;
+                case "BannerPapel":
+                    imvBannerMaterial.setImageResource(R.drawable.banner_papel);
+                    break;
+                case "BannerOrganico":
+                    imvBannerMaterial.setImageResource(R.drawable.banner_lixoorganico);
+                    break;
+                case "BannerHospitalar":
+                    imvBannerMaterial.setImageResource(R.drawable.banner_lixohospitalar);
+                    break;
+                default:
+                    // Lógica para tratar banners desconhecidos
+                    break;
+            }
+        }
     }
 }
